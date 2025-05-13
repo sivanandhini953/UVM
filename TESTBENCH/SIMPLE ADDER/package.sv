53050014c017c4ed679475819b9fb47ebeba5ce0
sivanandhini953 <sivanandhinis69@gmail.com>
2025-05-13 15:01:30 +0530

    Create package.sv

---
+++ TESTBENCH/SIMPLE ADDER/package.sv
@@ -0,0 +1,8 @@
+`include "sequence_item.sv"
+`include "sequence.sv"
+`include "sequencer.sv"
+`include "driver.sv"
+`include "monitor.sv"
+`include "scoreboard.sv"
+`include "agent.sv"
+`include "environment.sv"
