9a6b804faf43964b72e8be02f06adeb8f5a55b1b
sivanandhini953 <sivanandhinis69@gmail.com>
2025-05-20 16:30:10 +0530

    Create design.sv

---
+++ TESTBENCH/D_FF/design.sv
@@ -0,0 +1,18 @@
+// Code your design here
+module dff(
+input d,
+  input clk,
+  input reset,
+  output reg q
+
+);
+  
+  always @(posedge clk)
+    begin
+    if(reset)
+      q <= 0;
+  else 
+    q <= d;
+  
+    end
+endmodule
