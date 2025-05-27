02ce5a6b5b57219d5f90eff206d7208ad5a9ccd1
sivanandhini953 <sivanandhinis69@gmail.com>
2025-05-27 11:12:11 +0530

    Create sequencer.sv

---
+++ TESTBENCH/D_FF/sequencer.sv
@@ -0,0 +1,14 @@
+class seqncr extends uvm_sequencer#(seq_item);
+  
+  `uvm_component_utils(seqncr)
+  
+  function new(string name="seqncr",uvm_component parent=null);
+    super.new(name,parent);
+    
+  endfunction
+  
+
+  
+endclass
+  
+  
