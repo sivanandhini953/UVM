c181de50a57d422c8ff87032e9ed497add900422
sivanandhini953 <sivanandhinis69@gmail.com>
2025-05-27 11:14:15 +0530

    Update test.sv

--- TESTBENCH/D_FF/test.sv
+++ TESTBENCH/D_FF/test.sv
@@ -1,35 +1,42 @@
-`include "seq_item.sv"
-`include "sequence.sv"
-//`include "sequencer.sv"
-`include "driver.sv"
-`include "monitor.sv"
-`include "scoreboard.sv"
-`include "agent.sv"
-`include "environment.sv"
-
+`include "package.sv"
 class test extends uvm_test;
-  `uvm_component_utils(test)
-
-  seq sq;
-  env en;
-
+   `uvm_component_utils(test)
+  
+  
+  env env_o;
+  seq1 s1;
+  seq2 s2;
+  seq3 s3;
+ 
+  
   function new(string name = "test", uvm_component parent = null);
     super.new(name, parent);
+    
   endfunction
-
-  virtual function void build_phase(uvm_phase phase);
+  
+  function void build_phase(uvm_phase phase);
     super.build_phase(phase);
-    sq = seq::type_id::create("sq", this);
-    en = env::type_id::create("en", this);
+    env_o = env::type_id::create("env_o", this);
+    
+   // env.scr.done = env.agnt.drv.done;
+    
   endfunction
-
-  virtual task run_phase(uvm_phase phase);
+               
+               
+  task run_phase(uvm_phase phase);
+    s1 = seq1::type_id::create("s1");
+    s2 = seq2::type_id::create("s2");
+    s3 = seq3::type_id::create("s3");
+    
     phase.raise_objection(this);
-
-    sq.start(en.agnt.seqr);  // assuming env -> agent -> sequencer hierarchy
-
-    #60;
+   
+    s1.start(env_o.agnt.seqr);
+    #10;
+    s2.start(env_o.agnt.seqr);
+    #20;
+    s3.start(env_o.agnt.seqr);
+    
     phase.drop_objection(this);
+    
   endtask
-
 endclass
