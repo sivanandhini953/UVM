4fb234c08bdaab60abfb70b1bebaa1b703e1f86b
sivanandhini953 <sivanandhinis69@gmail.com>
2025-05-27 11:11:42 +0530

    Update seq_item.sv

--- TESTBENCH/D_FF/seq_item.sv
+++ TESTBENCH/D_FF/seq_item.sv
@@ -1,21 +1,23 @@
 class seq_item extends uvm_sequence_item;
+ 
   
+  rand bit reset;
   rand bit d;
-  bit clk;
-  bit reset;
   bit q;
   
+   `uvm_object_utils_begin(seq_item)
+  `uvm_field_int(reset,UVM_DEFAULT)
+  `uvm_field_int(d,UVM_DEFAULT)
+  `uvm_field_int(q,UVM_DEFAULT)
+  `uvm_object_utils_end
   
   function new(string name="seq_item");
     super.new(name);
-    endfunction 
+    
+  endfunction
+ 
+  constraint c1{reset dist {1:=1,0:=99};}
+  constraint c2{d dist {0:=50,1:=50};}
   
-  
-  `uvm_object_utils_begin(seq_item)
-  `uvm_field_int(d,UVM_DEFAULT);
-  `uvm_field_int(q,UVM_DEFAULT);
-  `uvm_object_utils_end
-  
-  
+
 endclass
-  
