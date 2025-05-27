d10af39b763432f31b7fd1443b41e9d13bfe0449
sivanandhini953 <sivanandhinis69@gmail.com>
2025-05-27 11:12:30 +0530

    Update driver.sv

--- TESTBENCH/D_FF/driver.sv
+++ TESTBENCH/D_FF/driver.sv
@@ -1,53 +1,46 @@
-class driver extends uvm_driver #(seq_item);
-  
-  `uvm_component_utils(driver)
-  
-  seq_item tr;
+ class driver extends uvm_driver#(seq_item);
+   `uvm_component_utils(driver)
+   event done;
   virtual intf vif;
   
+  
+  
   function new(string name="driver",uvm_component parent=null);
-    super.new(name,parent);
+
+   super.new(name,parent);
+    
   endfunction
   
-  task reset_dut();
-    vif.reset <= 1;
-    vif.d <= 0;
-    repeat(10) @(posedge vif.clk)
-      
-      vif.reset <= 0;
-    `uvm_info("DRV","reset done",UVM_NONE);
-  endtask
   
-  virtual function void build_phase(uvm_phase phase);
-    
+  function void build_phase(uvm_phase phase);
     super.build_phase(phase);
-    tr=seq_item::type_id::create("data");
-    
+    req=seq_item::type_id::create("req");
     if(!uvm_config_db #(virtual intf)::get(this,"","vif",vif))
-      `uvm_fatal("DRV","unable to access interface")
-      endfunction
-      
-      
-      virtual task run_phase(uvm_phase phase);
+    `uvm_fatal(get_type_name(),"not set at top level");
     
-    reset_dut();
+  endfunction
+  
+  task run_phase(uvm_phase phase);
     
     forever begin
+     
+       seq_item_port.get_next_item(req);
+       
+    
+       
+   	vif.reset = req.reset;
+      vif.d = req.d;
+       
       
-      seq_item_port.get_next_item(tr);
-      vif.d <= tr.d;
       
+       `uvm_info("drv",$sformatf("Driver to vif reset=%b , d:%b,  q:%b",vif.reset,vif.d,vif.q),UVM_NONE);
       seq_item_port.item_done();
-      `uvm_info("DRV", $sformatf("Trigger DUT d: %0d",tr.d),UVM_NONE);
-               @(posedge vif.clk);
-              @(posedge vif.clk);
-      @(posedge vif.clk);
-               
-               end
-               endtask
-               endclass
-               
+       
+       repeat(2) @(posedge vif.clk);
+       
+       //@ done;
+    end
+  endtask
+endclass
       
-      
-  
-  
+    
