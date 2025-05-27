13126e15ee0795874e4bb45d2ed452655af7a6d5
sivanandhini953 <sivanandhinis69@gmail.com>
2025-05-27 11:13:16 +0530

    Update scoreboard.sv

--- TESTBENCH/D_FF/scoreboard.sv
+++ TESTBENCH/D_FF/scoreboard.sv
@@ -1,36 +1,34 @@
 class scoreboard extends uvm_scoreboard;
- `uvm_component_utils(scoreboard)
+  event done;
+  seq_item data;
+  `uvm_component_utils(scoreboard)
 
-  uvm_analysis_imp #(seq_item, scoreboard) recv;
+  uvm_analysis_imp #(seq_item, scoreboard) item_collect_export;
 
-seq_item tr;
+  function new(string name = "scoreboard", uvm_component parent = null);
+    super.new(name, parent);
+    
+    item_collect_export = new("item_collect_export", this);
+  endfunction
 
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    data=seq_item::type_id::create("data");
+  endfunction
 
-
-  function new(string name = "scoreboard", uvm_component parent=null);
-    super.new(name,parent);
- recv = new("Read", this);
- endfunction
-
-
-
-virtual function void build_phase(uvm_phase phase);
-super.build_phase (phase);
-  tr=seq_item::type_id::create("tr");
- endfunction
-
-
-
-  virtual function void write(seq_item t);
-tr = t;
-    `uvm_info ("SCO", $sformatf("Data rcvd from Monitor d: %0d and q: %0d", tr.d, tr.q), UVM_NONE);
-
-    if(t.q == t.d) begin
-
-      `uvm_info("SCo", "Test Passed", UVM_NONE);end
- else begin
-   `uvm_info ("SCO", "Test Failed", UVM_NONE);end
- endfunction
-  
- 
- endclass
+  function void write(input seq_item t);
+    
+    if (t.reset) begin
+      `uvm_info("SCOREBOARD", " Reset active - skipping check", UVM_NONE);
+    end 
+    else begin
+      if (t.d == t.q)begin
+        `uvm_info("SCOREBOARD", "Test PASSED: Q matches D", UVM_NONE);
+      end
+      else begin
+        `uvm_error("SCOREBOARD", $sformatf(" Test FAILED: D = %0d, Q = %0d", t.d, t.q));
+    end
+    end
+    //-> done;
+  endfunction
+endclass
