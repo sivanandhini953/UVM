4bfe1e521a53f38b62c39b19317d93e49c440843
sivanandhini953 <sivanandhinis69@gmail.com>
2025-05-27 11:12:50 +0530

    Update monitor.sv

--- TESTBENCH/D_FF/monitor.sv
+++ TESTBENCH/D_FF/monitor.sv
@@ -1,44 +1,42 @@
-class monitor extends uvm_monitor;
-  `uvm_component_utils(monitor)
-  
-  
-  uvm_analysis_port #(seq_item) send;
-  seq_item tr;
+  class monitor extends uvm_monitor;
+   `uvm_component_utils(monitor)
+    
   virtual intf vif;
   
+  uvm_analysis_port #(seq_item) item_collect_port;
+  
+  seq_item trans;
+  
+ 
   
   function new(string name="monitor",uvm_component parent=null);
     super.new(name,parent);
-    send=new("write",this);
-    tr = seq_item:: type_id::create ("tr");
-  endfunction
-  
-  
-  
-  
-  virtual function void build_phase (uvm_phase phase);
-  
-  super.build_phase(phase);
     
-    if(!uvm_config_db #(virtual intf):: get(this, "", "vif", vif))
-      `uvm_error ("MON", "Unable to access uvm_config_db");
+    item_collect_port=new("item_collect_port",this);
+    
+    
   endfunction
   
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    trans=seq_item::type_id::create("trans");
+    if(!uvm_config_db #(virtual intf)::get(this,"","vif",vif))
+    `uvm_fatal(get_type_name(),"not set at top level");
+  endfunction
   
- virtual task run_phase (uvm_phase phase);
-   
-   wait(!vif.reset);
-          
-       @(posedge vif.clk);
-     tr.d <= vif.d;
-     tr.q <= vif.q;
-     tr.reset <= vif.reset;
-     tr.clk <= vif.clk;
-     
-     `uvm_info("MON", $sformatf("Data send to Scoreboard d: %0d and q: %0d", tr.d, tr.q), UVM_NONE);
-     send.write(tr);
-     
-   
- endtask
-endclass
+    task run_phase(uvm_phase phase);
+      
+      forever begin 
+        
+        repeat(2) @(posedge vif.clk);
        
+        trans.reset = vif.reset;
+        trans.d = vif.d;
+        trans.q = vif.q;
+      `uvm_info("mon",$sformatf("monitor to scoreboard reset=%b , d:%b,  q:%b",trans.reset,trans.d,trans.q),UVM_NONE);
+      item_collect_port.write(trans);
+        
+      end
+      
+endtask
+endclass
