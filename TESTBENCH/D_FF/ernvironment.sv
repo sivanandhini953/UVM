9eb9dc88b5ac3e97cf32d582d0c5bef407003f8a
sivanandhini953 <sivanandhinis69@gmail.com>
2025-05-27 11:13:58 +0530

    Update ernvironment.sv

--- TESTBENCH/D_FF/ernvironment.sv
+++ TESTBENCH/D_FF/ernvironment.sv
@@ -1,23 +1,24 @@
-class env extends uvm_env;
-  `uvm_component_utils(env)
-
-  function new(string name = "env", uvm_component parent=null);
+class env extends uvm_env;     //class creation
+  
+  `uvm_component_utils(env)    //factory registration
+  
+  agent agnt;                  //subclasses for environment  instantiation
+  scoreboard sb;
+  
+  
+  function new(string name="env",uvm_component parent=null);  //constructor
     super.new(name,parent);
-endfunction
-
-scoreboard sco;
- agent agnt;
-
- virtual function void build_phase(uvm_phase phase);
- super.build_phase (phase);
-   sco = scoreboard:: type_id::create("sco", this);
-   agnt = agent:: type_id:: create ("agnt", this);
- endfunction
-
-
- virtual function void connect_phase (uvm_phase phase);
-super.connect_phase (phase);
-   agnt.mon.send.connect(sco.recv);
- endfunction
-
- endclass
+    
+  endfunction
+  
+  function  void build_phase(uvm_phase phase);    //build phase
+    super.build_phase(phase);
+    agnt=agent::type_id::create("agnt",this);   //subclass memory creation
+    sb=scoreboard::type_id::create("sb",this);
+  endfunction
+  
+  function void connect_phase(uvm_phase phase);
+    super.connect_phase(phase); 
+    agnt.mon.item_collect_port.connect(sb.item_collect_export);
+  endfunction
+endclass
