6ea90d422323306c042b12563014dc2c0f471ce1
sivanandhini953 <sivanandhinis69@gmail.com>
2025-05-27 11:15:58 +0530

    Update sequence.sv

--- TESTBENCH/D_FF/sequence.sv
+++ TESTBENCH/D_FF/sequence.sv
@@ -1,28 +1,37 @@
-class seq extends uvm_sequence #(seq_item);
+class seq3 extends uvm_sequence#(seq_item);
+  `uvm_object_utils(seq3)
   
-  `uvm_object_utils(seq)
+ 
+    
   
-  seq_item tr;
-  
-  function new(string name="seq");
+  function new(string name="seq3");
     super.new(name);
+   // `uvm_info("sequence_CLASS","constructor",UVM_MEDIUM);
   endfunction
   
   
-  virtual task body();
-    
-    tr=seq_item::type_id::create("tr");
-    
-    repeat(10)
-      begin
-        
-        wait_for_grant();
-        void'(tr.randomize());
-        send_request(tr);
+ virtual task body();
+   
+   req= seq_item::type_id::create("req");
+   
+   
+     repeat(10) begin
+       
+       start_item(req);
+       void'(req.randomize());
+       
+       `uvm_info("SEQ3","data sent to driver",UVM_NONE);
+       req.print();
+       finish_item(req);
+       
+    /*wait_for_grant();
+       
+      void'(seqi.randomize());
+       `uvm_info("SEQUENCE",$sformatf("Randomized input d:%b",seqi.d),UVM_NONE);
+       send_request(seqi);
       wait_for_item_done();
-        
-        `uvm_info("GEN",$sformatf("data to DRV: d=%0d",tr.d),UVM_NONE);
-        
-      end
-  endtask
+       get_response(seqi);
+       `uvm_info("SEQUENCE","sequence completed",UVM_NONE);*/
+    end 
+  endtask 
 endclass
